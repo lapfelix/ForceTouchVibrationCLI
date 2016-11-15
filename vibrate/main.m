f2279a30e86bea75e217be9c7109ca31baa019ef
Felix Lapalme <lap.felix@gmail.com>
2016-11-14 22:44:31 -0500

    Initial Commit

---
+++ vibrate/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  vibrate
+//
+//  Created by Felix Lapalme on 2016-11-14.
+//  Copyright © 2016 Felix Lapalme. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
